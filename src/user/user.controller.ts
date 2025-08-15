9ccc08d016b2ef1c650f9a127e59bc9855e9eb38
rian-124 <afriyanlionel0@gmail.com>
2025-08-15 14:12:17 +0700

    disable response choose loket response data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -28,10 +28,7 @@ import { RegisterUserDto } from './dtos/register-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { PermissionsGuard } from 'src/common/guards/permissions.guard';
 import { Permissions } from 'src/common/decorators/permission.decorator';
-import {
-  UpdateLoketUserDto,
-  UpdateLoketUserResponse,
-} from './dtos/updateLoket-user';
+import { UpdateLoketUserDto } from './dtos/updateLoket-user';
 
 @ApiTags('Users')
 @ApiBearerAuth('access-token')
@@ -118,10 +115,10 @@ export class UserController {
   async updateUserByLokets(
     @Req() request: AuthenticatedRequest,
     @Body() body: UpdateLoketUserDto,
-  ): Promise<WebResponse<UpdateLoketUserResponse>> {
-    const result = await this.userService.updateUserByLoket(request, body);
+  ): Promise<WebResponse<Users>> {
+    await this.userService.updateUserByLoket(request, body);
 
-    return ResponseHelper.ok('Successfully update user loket', result);
+    return ResponseHelper.ok('Successfully update user loket');
   }
 
   @Put('/loket/checkout')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,10 +17,7 @@ import { Users } from '@prisma/client';
 import { WebSocketGateaway } from 'src/common/websocket.gateaway';
 import { RegisterUserDto } from './dtos/register-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import {
-  UpdateLoketUserDto,
-  UpdateLoketUserResponse,
-} from './dtos/updateLoket-user';
+import { UpdateLoketUserDto } from './dtos/updateLoket-user';
 
 @Injectable()
 export class UserService {
@@ -164,7 +161,7 @@ export class UserService {
   async updateUserByLoket(
     request: AuthenticatedRequest,
     body: UpdateLoketUserDto,
-  ): Promise<UpdateLoketUserResponse> {
+  ): Promise<Users> {
     const users = await this.prismaService.users.findUnique({
       where: {
         id: request.user.sub,
@@ -209,16 +206,7 @@ export class UserService {
       data: {
         loket_id: body.loket_id,
       },
-      select: {
-        lokets: {
-          select: {
-            nama_loket: true,
-          },
-        },
-      },
     });
-
-    this.wsGateaway.broadcastToAdmin(userLoketUpdate);
 
     return userLoketUpdate;
   }
@@ -250,8 +238,6 @@ export class UserService {
         loket_id: null,
       },
     });
-
-    this.wsGateaway.broadcastToAdmin(checkoutUserByLoket);
 
     return checkoutUserByLoket;
   }
